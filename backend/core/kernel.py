737ed92deb8a635648478eab40bb98fc9940e9bd
yxexe317 <Dahengeming09@gmail.com>
2026-03-15 17:28:10 +0300

    Add new frontend domain to CORS

--- backend/core/kernel.py
+++ backend/core/kernel.py
@@ -75,6 +75,7 @@ app.add_middleware(
     allow_origins=[
         "http://localhost:5173",
         "https://lucy-os.vercel.app"
+        "https://lucy-os-two.vercel.app"
     ],
     allow_credentials=True,
     allow_methods=["*"],
